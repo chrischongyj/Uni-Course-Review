1467c51b4c0ca7b72914da232994e7f2e6cada4b
chrischongyj <chrischongyj@gmail.com>
2020-04-22 19:20:37 +0800

    Added nuscourses.json

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', function (req, res) {
   res.sendFile('/index.html');
 });
 
+app.get('/404', function (req, res) {
+  res.sendFile(__dirname + '\\public\\404.html');
+});
+
 // About page route.
 app.get('/contact', function (req, res) {
   res.sendFile(__dirname + '\\public\\contact.html');
@@ -18,6 +22,7 @@ app.get('/contact', function (req, res) {
 
 app.get('/course/:course', function (req, res) {
   req.params.course = req.params.course.toUpperCase();
+  var isValid = false;
   fetch('https://api.nusmods.com/v2/2019-2020/moduleList.json')
   .then(response => {
     return response.json()
@@ -28,18 +33,43 @@ app.get('/course/:course', function (req, res) {
 
       if (data[i].moduleCode == req.params.course) {
         res.render('course', {course: req.params.course, coursename: data[i].title});
+        isValid = true;
         return;
       }
-      res.sendFile(__dirname + '\\public\\contact.html')
+    }
+  })
+  .catch(err => {
+    // Do something for an error here
+    console.log(err)
+  })
+
+  fetch('https://raw.githubusercontent.com/chrischongyj/Uni-Course-Review/master/public/ntucourses.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+
+    for (var i = 0; i < data.length; i++) {
+
+      if (data[i].FIELD1 == req.params.course) {
+        res.render('course', {course: req.params.course, coursename: data[i].FIELD2});
+        isValid = true;
+        return;
+      }
+
     }
 
   })
   .catch(err => {
     // Do something for an error here
     console.log(err)
-    res.sendFile(__dirname + '\\public\\contact.html')
   })
   
+
+
 });
+
+
 
 app.listen(3000, ()=>console.log('Listening at port 3000...'));

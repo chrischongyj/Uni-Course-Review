d4088cb48424ea95e1d0592b4ab11099bb5c0135
chrischongyj <chrischongyj@gmail.com>
2020-04-22 21:36:08 +0800

    Added ntu sem 1 courses

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const fetch = require("node-fetch");
 
 app.set('view engine', 'ejs');
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 // Home page route.
 app.get('/', function (req, res) {
-  res.sendFile('/index.html');
+  res.sendFile('index.html');
 });
 
 app.get('/404', function (req, res) {
-  res.sendFile(__dirname + '\\public\\404.html');
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 // About page route.
 app.get('/contact', function (req, res) {
-  res.sendFile(__dirname + '\\public\\contact.html');
+  res.sendFile(__dirname + '/public/contact.html');
 });
 
 app.get('/course/:course', function (req, res) {
@@ -66,6 +66,29 @@ app.get('/course/:course', function (req, res) {
     // Do something for an error here
     console.log(err)
   })
+
+  fetch('https://raw.githubusercontent.com/chrischongyj/Uni-Course-Review/master/public/ntucourses2.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+
+    for (var i = 0; i < data.length; i++) {
+
+      if (data[i].FIELD1 == req.params.course) {
+        res.render('course', {course: req.params.course, coursename: data[i].FIELD2});
+        isValid = true;
+        return;
+      }
+
+    }
+
+  })
+  .catch(err => {
+    // Do something for an error here
+    console.log(err)
+  })
   
 
 
@@ -73,4 +96,6 @@ app.get('/course/:course', function (req, res) {
 
 
 
-app.listen(process.env.PORT, ()=>console.log('Listening...'));
+app.listen(3000, ()=>console.log('Listening...'));
+
+// process.env.PORT

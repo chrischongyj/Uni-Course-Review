20a795029c515991e5694fe9b8b03c97170df9c0
chrischongyj <chrischongyj@gmail.com>
2020-04-22 21:39:14 +0800

    Fixed listening port

--- app.js
+++ app.js
@@ -96,6 +96,6 @@ app.get('/course/:course', function (req, res) {
 
 
 
-app.listen(3000, ()=>console.log('Listening...'));
+app.listen(process.env.PORT, ()=>console.log('Listening...'));
 
 // process.env.PORT
